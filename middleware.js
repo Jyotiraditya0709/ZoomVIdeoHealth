9e659b1e6062aefefae0dcccef097aaad52c946c
Jmishra49 <mishrajyotiraditya49@gmail.com>
2025-07-03 17:46:15 +0530

    made a middleware

---
+++ middleware.js
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+
+/**
+ * @param {import("next/server").NextRequest} _request
+ */
+export function middleware(_request) {
+  const csp = [
+    "default-src 'self'",
+    "script-src 'self' 'unsafe-inline'",
+    "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+    "font-src 'self' https://fonts.gstatic.com",
+  ].join("; ");
+
+  const response = NextResponse.next();
+  response.headers.set("Content-Security-Policy", csp);
+  return response;
+}
+
+export const config = {
+  matcher: "/(.*)", // apply to all routes
+};
