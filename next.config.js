1219e45868f3c25817f2f904baee2dfdd43576be
Jmishra49 <mishrajyotiraditya49@gmail.com>
2025-07-03 17:02:48 +0530

    Add CSP headers to allow Google Fonts

--- next.config.js
+++ next.config.js
@@ -5,18 +5,31 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
+
+// Define the CSP policy
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-inline' 'unsafe-eval';
+  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+  style-src-elem 'self' https://fonts.googleapis.com;
+  font-src 'self' https://fonts.gstatic.com;
+`;
+
+const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+  },
+];
+
 const config = {
   reactStrictMode: true,
 
-  /**
-   * If you are using `appDir` then you must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
   },
+
   images: {
     remotePatterns: [
       {
@@ -25,6 +38,15 @@ const config = {
         pathname: "**/*",
       },
     ],
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
   },
 };
 
